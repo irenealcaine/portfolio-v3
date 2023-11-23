2111fb25ea191e2cfb5454abde8650275fb767b8
ialcaine <101673351+irenealcaine@users.noreply.github.com>
2023-11-23 13:13:47 +0000

    Update 4 files

--- src/Components/Subtitle/Subtitle.jsx
+++ src/Components/Subtitle/Subtitle.jsx
@@ -2,7 +2,7 @@ import styles from "../../app/page.module.css";
 import "./Subtitle.css";
 
 const Subtitle = ({ subtitle }) => {
-  return <h1 className="subtitle">{subtitle}</h1>;
+  return <h2 className="subtitle">{subtitle}</h2>;
 };
 
 export default Subtitle;
