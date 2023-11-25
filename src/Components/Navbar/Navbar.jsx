e4fa5025f67966057d85da1de6ba2988c03a50e0
Irene Alcaine <irenealcainealvarez@gmail.com>
2023-11-25 17:17:18 +0100

    build(deps): update package-lock.json, package.json, Navbar.jsx and Navbar.scss

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,6 +2,8 @@
 import { useContext } from "react";
 import "./Navbar.scss";
 import { DarkModeContext } from "../../Context/darkModeContext";
+import { IoHomeOutline, IoMailOutline, IoSunnyOutline } from "react-icons/io5";
+import { FaLaptopCode } from "react-icons/fa";
 
 const Navbar = () => {
   const { dispatch } = useContext(DarkModeContext);
@@ -9,17 +11,25 @@ const Navbar = () => {
     <nav className="navbar">
       <ul>
         <li>
-          <a href="#home">🍕</a>
+          <a href="#home">
+            <IoHomeOutline />
+          </a>
         </li>
         <li>
-          <a href="#portfolio">🥙</a>
+          <a href="#portfolio">
+            <FaLaptopCode />
+          </a>
         </li>
         <li>
-          <a href="#contact">🥟</a>
+          <a href="#contact">
+            <IoMailOutline />
+          </a>
         </li>
       </ul>
       <ul>
-        <li onClick={() => dispatch({ type: "TOGGLE" })}>☀️</li>
+        <li onClick={() => dispatch({ type: "TOGGLE" })}>
+          <IoSunnyOutline />
+        </li>
       </ul>
     </nav>
   );
