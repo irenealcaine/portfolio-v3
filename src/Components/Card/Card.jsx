a3aebee023e7a89a07a695602918f83a1f43d1a1
ialcaine <101673351+irenealcaine@users.noreply.github.com>
2023-11-27 07:43:38 +0000

    Update 3 files

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,16 +1,32 @@
-import React from "react";
-import Image from "next/image";
+"use client";
 import "./Card.scss";
+import React, { useState } from "react";
+import Image from "next/image";
 import logo from "../../../public/next.svg";
 
 const Card = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <div className={`card`}>
-      <h3>Title asgasg asfsys tjtjdhdh</h3>
-      <img
-        src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg"
-        alt="g"
-      />
+      <h3>Title</h3>
+      {isOpen ? (
+        <div>
+          <img
+            src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg"
+            alt="g"
+          />
+        </div>
+      ) : (
+        <div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas
+            delectus praesentium consectetur repellendus fuga, debitis corporis
+            perferendis ea expedita ad molestias consequatur, voluptate
+            voluptatem, sed minus qui mollitia. Culpa, magni?
+          </p>
+        </div>
+      )}
+
       {/* <Image
         alt="Mountains"
         src={logo}
@@ -21,12 +37,6 @@ const Card = () => {
           objectFit: "cover",
         }}
       /> */}
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas delectus
-        praesentium consectetur repellendus fuga, debitis corporis perferendis
-        ea expedita ad molestias consequatur, voluptate voluptatem, sed minus
-        qui mollitia. Culpa, magni?
-      </p>
     </div>
   );
 };
--- src/Pages/Portfolio/Portfolio.jsx
+++ src/Pages/Portfolio/Portfolio.jsx
@@ -1,3 +1,4 @@
+import "./Portfolio.scss";
 import Card from "@/Components/Card/Card";
 import PageContainer from "@/Components/PageConainer/PageConainer";
 import Subtitle from "@/Components/Subtitle/Subtitle";
