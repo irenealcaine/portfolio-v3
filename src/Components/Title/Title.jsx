392b8964941aad78de8531a653269328bac48689
ialcaine <101673351+irenealcaine@users.noreply.github.com>
2023-11-28 10:57:19 +0000

    Update Title.css and Title.jsx

--- src/Components/Title/Title.jsx
+++ src/Components/Title/Title.jsx
@@ -4,6 +4,7 @@ import React, { useState, useEffect } from "react";
 
 const Title = ({ title }) => {
   const [visibleLetters, setVisibleLetters] = useState([]);
+  const colors = ["pink", "blue", "green"];
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -21,14 +22,19 @@ const Title = ({ title }) => {
 
   return (
     <h1 className="title">
-      {title.split("").map((char, index) => (
-        <span
-          key={index}
-          className={visibleLetters.includes(index) ? "visible" : ""}
-        >
-          {char === " " ? "\u00A0" : char}
-        </span>
-      ))}
+      {title.split("").map((char, index) => {
+        const charColor = colors[index % colors.length];
+        return (
+          <span
+            key={index}
+            className={`${
+              visibleLetters.includes(index) ? "visible" : ""
+            } ${charColor}`}
+          >
+            {char === " " ? "\u00A0" : char}
+          </span>
+        );
+      })}
     </h1>
   );
 };
