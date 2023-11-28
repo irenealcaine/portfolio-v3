6a599b599a465e3d6dfc49982c9805a520853ebf
ialcaine <101673351+irenealcaine@users.noreply.github.com>
2023-11-28 11:24:24 +0000

    Update 5 files

--- src/Components/Title/Title.jsx
+++ src/Components/Title/Title.jsx
@@ -4,7 +4,7 @@ import React, { useState, useEffect } from "react";
 
 const Title = ({ title }) => {
   const [visibleLetters, setVisibleLetters] = useState([]);
-  const colors = ["pink", "blue", "green"];
+  const colors = ["pink", "blue", "green", "red"];
 
   useEffect(() => {
     const interval = setInterval(() => {
